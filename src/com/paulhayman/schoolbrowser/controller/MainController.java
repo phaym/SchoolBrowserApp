b49fb8a4b65f428d77f99419ffc3c4be60f68e48
phaym <paul.haym@gmail.com>
2015-02-26 21:21:49 -0500

    -spring mvc configured with java list displayed

--- src/com/paulhayman/schoolbrowser/controller/MainController.java
+++ src/com/paulhayman/schoolbrowser/controller/MainController.java
@@ -1,21 +1,51 @@
 package com.paulhayman.schoolbrowser.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
-public class MainController extends AbstractController {
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
-			HttpServletResponse arg1) throws Exception {
+@Controller
+@RequestMapping("/Courses")
+public class MainController  {
+
+	@RequestMapping("/Display")
+	public ModelAndView displayHome() {
 
 		ModelAndView modelandview = new ModelAndView("HelloPage");
 		modelandview.addObject("welcomeMessage", "HELLO THIS IS AN APPLICATION");
 		
 		return modelandview;
+	}
+	
+	@RequestMapping("/StudentListing")
+	public ModelAndView displayStudentListing(){
+		List<Student> studentListing = new ArrayList<Student>();
+		
+		studentListing.add(new Student("Paul"));
+		studentListing.add(new Student("Mark"));
+		studentListing.add(new Student("Tom"));
+
+		ModelAndView model = new ModelAndView("StudentListing");
+		model.addObject("studentListing", studentListing);
+		return model;
+	}
+	
+	@ModelAttribute
+	public void addAttributeToAllModels(Model model){
+	
+	}
+	@InitBinder
+	public void customDataBinder(WebDataBinder binder){
+		
 	}
 
 }
